c40d6252ffb1cbf360ecf270099629c88db0b977
Aashna Kulshrestha <aashna.kulsh@gmail.com>
2023-12-04 21:39:02 -0500

    beatable chunk

--- levelsetup2.py
+++ levelsetup2.py
@@ -4,7 +4,7 @@ import random
 
 app.width = 1450
 app.height = 800
-app.blockLength = 50
+app.blockLength = 40
 app.totalBlocksInRow = math.ceil(app.width/app.blockLength)
 app.totalBlocksInCol = math.ceil(app.height/app.blockLength)
 app.groundHeight = app.height
@@ -65,13 +65,24 @@ def generateChunk(chunk):
         drawRect(block.posxTL, block.posyTL, app.blockLength, app.blockLength, 
                  fill = block.color, border = 'black', opacity = 50)
 
+#TODO: REMOVE AFTER TESTING
+blockcol = set()
+for y in range(0, app.totalBlocksInCol) :
+    blockcol.add(Block(0, y, 'dirt'))
 defaultChunk1 = (
-            createBlockRow(0, app.totalBlocksInRow, 3, 'grass') |
+            createBlockRow(0, app.totalBlocksInRow, 4, 'grass') |
+            createBlockRow(0, app.totalBlocksInRow, 3, 'dirt') |
             createBlockRow(0, app.totalBlocksInRow, 2, 'dirt') |
             createBlockRow(0, app.totalBlocksInRow, 1, 'dirt') |
-            createBlockRow(0, app.totalBlocksInRow, 0, 'dirt')) 
+            createBlockRow(0, app.totalBlocksInRow, 0, 'dirt') | blockcol) 
 
-defaultChunk1Collidable = createBlockRow(0, app.totalBlocksInRow, 3, 'grass')
+
+#TODO
+def getCollidableBlocks(chunk):
+    pass
+
+# defaultChunk1Collidable = getCollidableBlocks(defaultChunk1)
+defaultChunk1Collidable = createBlockRow(0, app.totalBlocksInRow, 4, 'grass')
 
 def getGroundHeightIndex(chunk):
     for block in chunk:
@@ -90,16 +101,15 @@ class Hole:
         self.length = random.randint(1, 5) #1 - total blocks in row
         self.yIndex = groundHeightIndex
         self.height = random.randint(1, groundHeightIndex) #1 - ground height
-        self.itemBottomOfHole = random.choices(['garbage', 'ooze', 'empty'], [40, 20, 40])
-        self.isPlatform = random.choices([True, False], [70, 30])
-        if self.isPlatform:
+        self.itemBottom = random.choices(['garbage', 'ooze', 'empty'], [40, 20, 40])[0]
+        self.hasPlatform = random.choices([True, False], [70, 30])
+        if self.hasPlatform:
             self.platformHeight = random.randint(groundHeightIndex, groundHeightIndex+3) #ground height - total blocks in col
-            self.item = random.choices(['smog', 'empty', 'life', 'invincibility'], [30, 30, 30, 10])
+            self.itemPlatform = random.choices(['smog', 'empty', 'life', 'invincibility'], [30, 30, 30, 10])[0]
             self.itemXIndex = random.randint(self.xIndex, self.xIndex + self.length)
         else:
             self.platformHeight = None
-            self.item = None
-    # return holeIndex, holeLength, holeHeight, itemBottomOfHole, isPlatform, platformHeight, itemOnPlatform
+            self.itemPlatform = None
 
 def addHolesToChunks(chunk):
     blocksToRemove = set()
@@ -117,32 +127,46 @@ def addHolesToChunks(chunk):
                 hole.yIndex >= block.yIndex >= hole.yIndex - hole.height):
                 blocksToRemove.add(block)
         
-        if hole.isPlatform:
+        #spawn garbage at bottom of hole
+        if hole.itemBottom == 'garbage':
             for i in range(hole.length+1):
-                print(hole.xIndex + i)
+                blocksToAdd.add(Block(hole.xIndex + i, hole.yIndex - hole.height, 'garbage'))
+        
+        #spawn ooze monster at bottom of hole
+        elif hole.itemBottom == 'ooze':
+            blocksToAdd.add(Block(hole.itemXIndex, hole.yIndex - hole.height, 'ooze'))
+
+        #create platform
+        if hole.hasPlatform:
+            for i in range(hole.length+1):
                 blocksToAdd.add(Block(hole.xIndex + i, hole.yIndex + hole.platformHeight, 'platform'))
 
-            if str(hole.item) == ['smog']:
+            #spawn smog monster on platform
+            if str(hole.itemPlatform) == 'smog':
                 blocksToAdd.add(Block(hole.itemXIndex, hole.yIndex + hole.platformHeight + 1, 'smog'))
             
-            if hole.item == ['life']:
+            #spawn life powerup on platform
+            elif hole.itemPlatform == 'life':
                 blocksToAdd.add(Block(hole.itemXIndex, hole.yIndex + hole.platformHeight + 1, 'life'))
             
-            if hole.item == ['invincibility']:
+            #spawn invincibility powerup on platform
+            elif hole.itemPlatform == 'invincibility':
                 blocksToAdd.add(Block(hole.itemXIndex, hole.yIndex + hole.platformHeight + 1, 'invincibility'))
 
-
-    return (chunk-blocksToRemove) | blocksToAdd
+    newChunk = (chunk-blocksToRemove) | blocksToAdd
+    return newChunk
 
 def findOverlappingBounds(holes):
-    # Sort holes based on x_index
+    # Finds overlapping bounds for holes, returning the overall bounds
+
+    # Sort holes based on xIndex
     sortedHoles = sorted(holes, key=lambda hole: hole.xIndex)
 
     # Initialize variables
     start = sortedHoles[0].xIndex
     end = sortedHoles[0].xIndex + sortedHoles[0].length
 
-    # Lists to store the results
+    # Lists to store  results
     overlappingBounds = []
 
     # Iterate through sorted holes to find the overlapping bounds
@@ -161,8 +185,29 @@ def findOverlappingBounds(holes):
 
 def isChunkBeatable(chunk, holes):
     #chunk is a set, holes is a list
-    
-    pass
+    holeBounds = findOverlappingBounds(holes)
+
+    for hole in holes:
+        # Hole has a platform
+        if hole.hasPlatform:
+            # Platform is TOO HIGH for player to jump onto
+            if hole.platformHeight > 15:
+                return False
+            # Player CAN jump onto platform
+            else: return True
+
+        # Hole has NO platform
+        else:
+            for x, y in holeBounds:
+            # Hole is too long for player to jump over
+                if y-x > 9: 
+                    # Hole has garbage/ooze monster at bottom
+                    if hole.itemBottom != 'empty': return False
+                    # Hole does NOT have garbage/ooze monster at bottom
+                    else: return True
+                # Player can jump over hole
+                else: return True
+        
 
 def generateLevel(defaultChunk):
     pass
